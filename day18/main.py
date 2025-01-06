5d02ce5338673650fca2f49e9efbd288cd95a2bd
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-07 00:54:30 +0100

    feat: part 1

--- day18/main.py
+++ day18/main.py
@@ -4,7 +4,7 @@ from collections import deque
 def parseData(name="task"):
     lines = open(f"{name}.txt").read().splitlines()
     coords = [line.split(",") for line in lines]
-    return [tuple((int(y), int(x))) for x, y in coords]
+    return [tuple((int(x), int(y))) for x, y in coords]
 
 
 def solve(data, kb):
@@ -15,10 +15,8 @@ def solve(data, kb):
     stack = deque([(0, 0, 0)])
     seen = []
 
-            
     while stack:
         r, c, s = stack.popleft()
-        seen.append((r, c))
 
         for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
             if nr < 0 or nc < 0 or nr > grid_size or nc > grid_size:
@@ -37,22 +35,11 @@ def solve(data, kb):
                 # reached end
                 return s + 1
 
+            seen.append((nr, nc))
             stack.append((nr, nc, s + 1))
-
-    # for r in range(grid_size + 1):
-    #     out = ""
-    #     for c in range(grid_size + 1):
-    #         if (r, c) in path:
-    #             out += "O"
-    #         elif (r, c) in grid:
-    #             out += "#"
-    #         else:
-    #             out += "."
-    #     print(out)
 
 
 print("🎄 Day 18: RAM Run")
 print("Part 1:", solve(parseData("sample"), 12))
-# < 310
-print("Part 1:", solve(parseData("task"), 1024))
+# print("Part 1:", solve(parseData("task"), 1024))
 # print(f"Part 2:", solve(parseData("sample")))
