fdbfc29f882a13059c4424dd3263847136d1243a
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-07 00:34:03 +0100

    feat: day 16

--- day16/main.py
+++ day16/main.py
@@ -1,4 +1,3 @@
-from collections import deque
 from heapq import heapify, heappop, heappush
 
 
@@ -7,7 +6,7 @@ def parseData(name="task"):
     return [list(line) for line in grid]
 
 
-def solve(grid):
+def solve1(grid):
     start, end = (0, 0), (0, 0)
     rows, cols = len(grid), len(grid[0])
     dirs = [(-1, 0, "N"), (1, 0, "S"), (0, 1, "E"), (0, -1, "W")]
@@ -23,11 +22,11 @@ def solve(grid):
                 end = pos
 
     visited = set()
-    pq = [(0, "E", start, 1)]
+    pq = [(0, "E", start)]
     heapify(pq)
 
     while pq:
-        score, facing, pos, spots = heappop(pq)
+        score, facing, pos = heappop(pq)
         r, c = pos
         visited.add((r, c, facing))
 
@@ -43,12 +42,68 @@ def solve(grid):
                 nScore = score + mod
 
                 if nPos == end:
-                    return (nScore, spots + 1)
+                    return nScore
                 else:
-                    heappush(pq, (nScore, dn, nPos, spots + 1))
+                    heappush(pq, (nScore, dn, nPos))
+
+
+def solve2(grid):
+    start, end = (0, 0), (0, 0)
+    rows, cols = len(grid), len(grid[0])
+    dirs = [(-1, 0, "N"), (1, 0, "S"), (0, 1, "E"), (0, -1, "W")]
+
+    # Discover all nodes
+    for r in range(rows):
+        for c in range(cols):
+            pos = (r, c)
+            t = grid[r][c]
+            if t == "S":
+                start = pos
+            elif t == "E":
+                end = pos
+
+    pq = [(0, "E", start, set())]
+    heapify(pq)
+
+    costs = {}
+    paths = set()
+
+    while pq:
+        score, facing, pos, path = heappop(pq)
+        r, c = pos
+
+        posCosts = costs.get((pos, facing), float("inf"))
+        if score > posCosts:
+            continue
+        elif score < posCosts:
+            costs[(pos, facing)] = score
+
+        path.add(pos)
+
+        for dr, dc, dn in dirs:
+            nr, nc = (dr + r, dc + c)
+            nPos = (nr, nc)
+            nt = grid[nr][nc]
+
+            if nt == "#":
+                continue
+
+            mod = 1 if facing == dn else 1001
+            nScore = score + mod
+
+            if nPos == end:
+                if nScore > costs.get(end, float("inf")):
+                    break
+               
+                path.add(end)
+                paths = paths.union(path)
+                costs[end] = nScore
+            else:
+                heappush(pq, (nScore, dn, nPos, path.copy()))
+
+    return len(path)
 
 
 print("🎄 Day 16: Reindeer Maze")
-print("Part 1:", solve(parseData("sample")))
-# print("Part 1:", solve(parseData("task")))
-# print(f"Part 2:", solve(parseData("sample")))
+print("Part 1:", solve1(parseData("sample")))
+print(f"Part 2:", solve2(parseData("sample")))
