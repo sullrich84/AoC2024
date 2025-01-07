d13b091cfa891943816deb1b808fd06336165976
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-07 22:58:29 +0100

    feat: day 18

--- day18/main.py
+++ day18/main.py
@@ -7,39 +7,53 @@ def parseData(name="task"):
     return [tuple((int(x), int(y))) for x, y in coords]
 
 
-def solve(data, kb):
-    grid_size = max([r for r, _ in data] + [c for _, c in data])
-    grid = data[:kb]
+def solve(part, data, kb):
+    size = max([r for r, _ in data] + [c for _, c in data])
+    start_grid = data[:kb]
 
-    # row, col, steps
-    stack = deque([(0, 0, 0)])
-    seen = []
+    def bfs(grid, sr, sc, er, ec):
+        stack = deque([(sr, sc, 0, [])])
+        seen = []
 
-    while stack:
-        r, c, s = stack.popleft()
+        while stack:
+            r, c, s, p = stack.popleft()
 
-        for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
-            if nr < 0 or nc < 0 or nr > grid_size or nc > grid_size:
-                # out of range
-                continue
+            for nr, nc in [(r + 1, c), (r, c + 1), (r - 1, c), (r, c - 1)]:
+                if nr < 0 or nc < 0 or nr > size or nc > size:
+                    # out of range
+                    continue
 
-            if (nr, nc) in grid:
-                # corrupted field
-                continue
+                if (nr, nc) in grid:
+                    # corrupted field
+                    continue
 
-            if (nr, nc) in seen:
-                # detour
-                continue
+                if (nr, nc) in seen:
+                    # detour
+                    continue
 
-            if nr == nc == grid_size:
-                # reached end
-                return s + 1
+                np = p.copy()
+                np.append((nr, nc))
 
-            seen.append((nr, nc))
-            stack.append((nr, nc, s + 1))
+                if nr == er and nc == ec:
+                    # reached end
+                    return np
+
+                seen.append((nr, nc))
+                stack.append((nr, nc, s + 1, np))
+
+    path = bfs(start_grid, 0, 0, size, size)
+    
+    if part == 1:
+        return len(path) 
+    
+    for i in range(len(data), 0, -1):
+        grid2 = data[:i]
+        path2 = bfs(grid2, 0, 0, size, size)
+        if path2:
+            r,c = data[i]
+            return f"{r},{c}" 
 
 
 print("🎄 Day 18: RAM Run")
-print("Part 1:", solve(parseData("sample"), 12))
-# print("Part 1:", solve(parseData("task"), 1024))
-# print(f"Part 2:", solve(parseData("sample")))
+print("Part 1:", solve(1, parseData("task"), 1024))
+print("Part 2:", solve(2, parseData("task"), 1024))
