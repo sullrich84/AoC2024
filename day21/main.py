15fa071c90fa0943bc618e2b68a3183ea6dc953e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-10 19:00:29 +0100

    feat: day21

--- day21/main.py
+++ day21/main.py
@@ -1,24 +1,25 @@
 from collections import deque
 from itertools import product
+from functools import cache
 
 
 def parseData(name="task"):
     return open(f"{name}.txt").read().splitlines()
 
 
-def solve(data):
+def solve(data, keypads):
     num_pad = [
         ["7", "8", "9"],
         ["4", "5", "6"],
         ["1", "2", "3"],
         ["X", "0", "A"],
     ]
-    
+
     arr_pad = [
         ["X", "^", "A"],
         ["<", "v", ">"],
     ]
-    
+
     def build_paths(pad):
         paths = {}
         rows, cols = len(pad), len(pad[0])
@@ -51,34 +52,50 @@ def solve(data):
                             paths[start_key][dest_key] = []
 
                         paths[start_key][dest_key].append(seq + key + "A")
-                        stack.append([nr, nc, seq + key])
+                        stack.append((nr, nc, seq + key))
         return paths
 
     num_paths = build_paths(num_pad)
     arr_paths = build_paths(arr_pad)
 
-    def get_instructions(string, paths):
+    def get_instructions(seq):
         possibilities = []
         prev = "A"
-        for s in list(string):
-            possibilities.append(paths[prev][s])
+        for s in list(seq):
+            possibilities.append(num_paths[prev][s])
             prev = s
         return ["".join(s) for s in list(product(*possibilities))]
 
+    @cache
+    def calc_length(src, dest, depth):
+        if depth == 1:
+            # Since every path has the same length
+            # we can work with first length only
+            return len(arr_paths[src][dest][0])
+
+        optimal = float("inf")
+        for seq in arr_paths[src][dest]:
+            length = 0
+            for nSrc, nDest in zip("A" + seq, seq):
+                length += calc_length(nSrc, nDest, depth - 1)
+            optimal = min(optimal, length)
+        return optimal
+
     complexity = 0
+
     for line in data:
-        sequences = get_instructions(line, num_paths)
-        for _ in range(2):
-            next_sequences = []
-            for seq in sequences:
-                next_sequences += get_instructions(seq, arr_paths)
-            minlen = min(map(len, next_sequences))
-            sequences = [seq for seq in next_sequences if len(seq) == minlen]
-        complexity += len(sequences[0]) * int(line[:-1])
-    
+        sequences = get_instructions(line)
+        optimal = float("inf")
+        for seq in sequences:
+            length = 0
+            for src, dest in zip("A" + seq, seq):
+                length += calc_length(src, dest, keypads)
+            optimal = min(optimal, length)
+        complexity += optimal * int(line[:-1])
+
     return complexity
 
 
 print("🎄 Day 21: Keypad Conundrum")
-print("Part 1:", solve(parseData("task")))
-# print(f"Part 2:", solve(parseData("sample")))
+print("Part 1:", solve(parseData("task"), 2))
+print(f"Part 2:", solve(parseData("task"), 25))
