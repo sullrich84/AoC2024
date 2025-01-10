3a7168e19afdceeed2e1bb8e62d9af27725895ef
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-10 22:35:33 +0100

    feat: day22

--- day22/main.py
+++ day22/main.py
@@ -1,32 +1,54 @@
 from functools import cache
 from math import floor
 
+
 def parseData(name="task"):
     lines = open(f"{name}.txt").read().splitlines()
     return [int(line) for line in lines]
 
 
 def solve(data):
+    def mix(x, y):
+        return x ^ y
 
-    @cache
-    def mix_prune(i, sn):
-        return (i ^ sn) % 16777216
+    def prune(x):
+        return x % 16777216
 
-    @cache
-    def sec_num(sn):
-        step1 = mix_prune(sn * 64, sn)
-        step2 = mix_prune(floor(step1 / 32), step1) 
-        step3 = mix_prune(step2 * 2048, step2)
-        return step3
-
-    results = []
-    for init_sn in data:
-        sn = init_sn
+    def prices(x):
+        ans = [x % 10]
         for _ in range(2000):
-            sn = sec_num(sn)
-        results.append(sn)
+            x = prune(mix(x * 64, x))
+            x = prune(mix(floor(x / 32), x))
+            x = prune(mix(x * 2048, x))
+            ans.append(x % 10)
+        return ans
 
-    return sum(results)
+    def changes(p):
+        return [p[i + 1] - p[i] for i in range(len(p) - 1)]
+
+    def score(prices, changes):
+        score = {}
+        for i in range(len(changes) - 3):
+            pattern = tuple(changes[i : i + 4])
+            if pattern not in score:
+                # We only care about the first occurence
+                score[pattern] = prices[i + 4]
+        return score
+
+    global_score = {}
+    for line in data:
+        p = prices(line)
+        c = changes(p)
+        s = score(p, c)
+        for pat, prc in s.items():
+            # Sum up all prices depending on their pattern
+            if pat not in global_score:
+                global_score[pat] = prc
+            else:
+                global_score[pat] += prc
+
+    best_price = max(global_score.values())
+    return best_price
 
 
 print("🎄 Day 22: Monkey Market")
