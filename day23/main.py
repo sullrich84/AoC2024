3040d7f3ca870c4ee73b9642516bd0d869752f0e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-11 09:28:52 +0100

    feat: day23

--- day23/main.py
+++ day23/main.py
@@ -31,18 +31,26 @@ def solve2(data):
         network[node1].add(node2)
         network[node2].add(node1)
 
-    cliques = set([])
-    for node, neighbours in network.items():
-        for n1 in neighbours:
-            for n2 in network[n1]:
-                if node in network[n2]:
-                    if "t" in [node[:1], n1[:1], n2[:1]]:
-                        cliques.add(tuple(sorted([node, n1, n2])))
+    def bron_kerbosch(r, p, x):
+        if not p and not x:
+            return r
+        max_clique = set()
+        for v in list(p):
+            clique = bron_kerbosch(r | {v}, p & network[v], x & network[v])
+            if len(clique) > len(max_clique):
+                max_clique = clique
+            p.remove(v)
+            x.add(v)
+        return max_clique
 
-    return len(cliques)
+    all_nodes = set(network.keys())
+    max_clique = bron_kerbosch(set(), all_nodes, set())
+    
+    return ",".join(tuple(sorted(max_clique)))
+
 
 
 
 print("🎄 Day 23: LAN Party")
 # print("Part 1:", solve1(parseData("task")))
-print("Part 2:", solve2(parseData("sample")))
+print("Part 2:", solve2(parseData("task")))
