150e4570ba94475e29265f510f0f4602ecfc9a1e
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-12 10:54:29 +0100

    feat: day14

--- day14/main.py
+++ day14/main.py
@@ -1,4 +1,3 @@
-from icecream import ic
 from math import floor
 
 
@@ -15,14 +14,6 @@ def parseData(name="task"):
     return (pos, vec)
 
 
-def print_map(pos, mr, mc):
-    for r in range(mr):
-        out = ""
-        for c in range(mc):
-            out += "X" if (r, c) in pos.values() else "."
-        print(out)
-
-
 def solve1(pos, vec, max_time, mr, mc):
     bots = [*pos.keys()]
 
@@ -30,8 +21,6 @@ def solve1(pos, vec, max_time, mr, mc):
         for bot in bots:
             (r, c), (dr, dc) = pos[bot], vec[bot]
             pos[bot] = ((r + dr) % mr, (c + dc) % mc)
-
-    print_map(pos, mr, mc)
 
     cline = floor(mc / 2)
     rline = floor(mr / 2)
@@ -54,25 +43,31 @@ def solve1(pos, vec, max_time, mr, mc):
     return q1 * q2 * q3 * q4
 
 
-
-
 def solve2(pos, vec, mr, mc):
     bots = [*pos.keys()]
-    rr = floor(mr / 2)
-    cc = floor(mc / 2)
+    ct_pos = set()
 
-    for time in range(25_000):
+    for time in range(8000):
+        ct_pos.clear()
         for bot in bots:
             (r, c), (dr, dc) = pos[bot], vec[bot]
-            pos[bot] = ((r + dr) % mr, (c + dc) % mc)
+            nr, nc = ((r + dr) % mr, (c + dc) % mc)
+            pos[bot] = (nr, nc)
+            ct_pos.add((nr, nc))
 
-        if time == 7093:
-            print_map(pos, mr, mc)
-            exit()
-       
+        conseq = 0
+        for r in range(mr):
+            for c in range(mc):
+                if (r, c) not in ct_pos:
+                    conseq = 0
+                    continue
+
+                conseq += 1
+                if conseq > 9:
+                    return time + 1
 
 
 print("🎄 Day 17: Chronospatial Computer")
 # print("Part 1:", solve1(*parseData("sample"), 100, 7, 11))
-# print("Part 1:", solve1(*parseData("task"), 100, 103, 101))
+print("Part 1:", solve1(*parseData("task"), 100, 103, 101))
 print("Part 2:", solve2(*parseData("task"), 103, 101))
