1216d59f9fff0cfca851587da667e4a351e781cf
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-12 01:56:27 +0100

    feat: day24 part2

--- day14/main.py
+++ day14/main.py
@@ -54,21 +54,25 @@ def solve1(pos, vec, max_time, mr, mc):
     return q1 * q2 * q3 * q4
 
 
-def solve2(pos, vec, max_time, mr, mc):
-    bots = [*pos.keys()]
 
-    for _ in range(max_time):
+
+def solve2(pos, vec, mr, mc):
+    bots = [*pos.keys()]
+    rr = floor(mr / 2)
+    cc = floor(mc / 2)
+
+    for time in range(25_000):
         for bot in bots:
             (r, c), (dr, dc) = pos[bot], vec[bot]
             pos[bot] = ((r + dr) % mr, (c + dc) % mc)
-            
 
-    print_map(pos, mr, mc)
-
-    return 0
+        if time == 7093:
+            print_map(pos, mr, mc)
+            exit()
+       
 
 
 print("🎄 Day 17: Chronospatial Computer")
 # print("Part 1:", solve1(*parseData("sample"), 100, 7, 11))
 # print("Part 1:", solve1(*parseData("task"), 100, 103, 101))
-print("Part 2:", solve2(*parseData("task"), 100, 103, 101))
+print("Part 2:", solve2(*parseData("task"), 103, 101))
