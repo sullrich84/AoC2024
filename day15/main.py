bda511458bdd5aceb9f4346433c0a9b9aa91d2af
Sebastian Ullrich <sebastian.ullrich@deepsource.de>
2025-01-19 23:11:10 +0100

    feat: day15

--- day15/main.py
+++ day15/main.py
@@ -1,5 +1,3 @@
-from icecream import ic
-
 dirs = {
     "^": (-1, 0),
     "v": (+1, 0),
@@ -8,9 +6,18 @@ dirs = {
 }
 
 
-def parseData(name="task"):
+def parseData(name="task", extend=False):
     grid, moves = open(f"{name}.txt").read().split("\n\n")
-    grid = [list(c) for c in grid.splitlines()]
+    grid = grid.splitlines()
+    if extend:
+        for i, row in enumerate(grid):
+            grid[i] = (
+                row.replace("#", "##")
+                .replace(".", "..")
+                .replace("@", "@.")
+                .replace("O", "[]")
+            )
+    grid = [list(c) for c in grid]
     moves = list(map(lambda c: dirs[c], list(moves.replace("\n", ""))))
     return grid, moves
 
@@ -23,95 +30,87 @@ def find(grid, char):
     return (-1, -1)
 
 
-def find_all(grid, char):
-    all = []
-    for r in range(len(grid)):
-        for c in range(len(grid[0])):
-            if grid[r][c] == char:
-                all.append((r, c))
-    return all
+def solve1(grid, moves):
+    rows, cols = len(grid), len(grid[0])
+    r, c = find(grid, "@")
 
+    for dr, dc in moves:
+        targets = [(r, c)]
+        cr = r
+        cc = c
+        go = True
+        while True:
+            cr += dr
+            cc += dc
+            char = grid[cr][cc]
+            if char == "#":
+                go = False
+                break
+            if char == "O":
+                targets.append((cr, cc))
+            if char == ".":
+                break
+        if not go:
+            continue
+        grid[r][c] = "."
+        grid[r + dr][c + dc] = "@"
+        for br, bc in targets[1:]:
+            grid[br + dr][bc + dc] = "O"
+        r += dr
+        c += dc
 
-def add(a, b):
-    return (a[0] + b[0], a[1] + b[1])
-
-
-def draw(grid, pos, walls, boxes):
-    for r in range(len(grid)):
-        out = ""
-        for c in range(len(grid[0])):
-            if (r, c) == pos:
-                out += "@"
-            elif (r, c) in boxes:
-                out += "O"
-            elif (r, c) in walls:
-                out += "#"
-            else:
-                out += "."
-        print(out)
-    print()
-
-
-def in_range(pos, rows, cols):
-    r, c = pos
-    return r in range(rows) and c in range(cols)
-
-
-def sum_coords(boxes):
     ans = 0
-    for r, c in boxes:
-        ans += 100 * r + c
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "O":
+                ans += 100 * r + c
+
     return ans
 
 
-def safe_guard(walls, boxes, pos):
-    if len(set(boxes)) != len(boxes):
-        raise Warning("Overlapping boxes")
-
-    if pos in boxes:
-        raise Warning("Robot overlapping with boxes")
-
-    if pos in walls:
-        raise Warning("Robot overlapping with walls")
-
-    for b in boxes:
-        if b in walls:
-            raise Warning("Box in Wall")
-
-
-def solve1(grid, moves):
+def solve2(grid, moves):
     rows, cols = len(grid), len(grid[0])
-    boxes = find_all(grid, "O")
-    walls = find_all(grid, "#")
-    pos = find(grid, "@")
+    r, c = find(grid, "@")
 
-    for mvec in moves:
-        npos = add(pos, mvec)
-        # safe_guard(walls, boxes, pos)
-
-        if not in_range(npos, rows, cols) or npos in walls:
+    for dr, dc in moves:
+        targets = [(r, c)]
+        go = True
+        for cr, cc in targets:
+            nr = cr + dr
+            nc = cc + dc
+            if (nr, nc) in targets:
+                continue
+            char = grid[nr][nc]
+            if char == "#":
+                go = False
+                break
+            if char == "[":
+                targets.append((nr, nc))
+                targets.append((nr, nc + 1))
+            if char == "]":
+                targets.append((nr, nc))
+                targets.append((nr, nc - 1))
+        if not go:
             continue
-        if npos in boxes:
-            to_move = []
-            search_pos = npos
-            # Scan for all adjacent boxes
-            while in_range(search_pos, rows, cols) and search_pos in boxes:
-                to_move.append(search_pos)
-                search_pos = add(search_pos, mvec)
-            # Check if last box can be moved to empty field
-            if add(to_move[-1], mvec) not in walls:
-                pos = npos
-                for m in reversed(to_move):
-                    boxes.remove(m)
-                    boxes.append(add(m, mvec))
-        else:
-            pos = npos
+        copy = [list(row) for row in grid]
+        grid[r][c] = "."
+        grid[r + dr][c + dc] = "@"
+        for br, bc in targets[1:]:
+            grid[br][bc] = "."
+        for br, bc in targets[1:]:
+            grid[br + dr][bc + dc] = copy[br][bc]
+        r += dr
+        c += dc
 
-    # ic(mvec)
-    draw(grid, pos, walls, boxes)
-    return sum_coords(boxes)
+    ans = 0
+    for r in range(rows):
+        for c in range(cols):
+            if grid[r][c] == "[":
+                ans += 100 * r + c
+
+    return ans
 
 
 print("🎄 Day 15: Warehouse Woes")
-print("Part 1:", solve1(*parseData("task")))
-# print("Part 2:", solve(*parseData("sample")))
+print("Part 1:", solve1(*parseData("task", extend=False)))
+print("Part 2:", solve2(*parseData("task", extend=True)))
